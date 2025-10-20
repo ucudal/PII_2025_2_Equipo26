3cf94438b68f29ebf6e2eb50a3a9b5d537faa3b6
Anthony-Vi <anthony.viera@correo.ucu.edu.uy>
2025-10-19 21:12:19 -0300

    se fusiona con otra rama para estar al dia

--- test/Library.Tests/FachadaTest.cs
+++ test/Library.Tests/FachadaTest.cs
@@ -1,58 +1,9 @@
-namespace Library.Tests;
-using Library;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+namespace Library.Tests
+{
+    using Library;
+}
+
 
 namespace Library.Tests
 {
-    [TestClass]
-    public class FachadaTests
-    {
-        [TestMethod]
-        public void CrearCliente_AumentaLaCantidadDeClientesEnUno()
-        {
-            // Arrange
-            var fachada = new Fachada();
-            int cantidadInicial = fachada.VerTodosLosClientes().Count;
-
-            // Act
-            fachada.CrearCliente("Carlos", "Perez", "123", "cp@test.com");
-            int cantidadFinal = fachada.VerTodosLosClientes().Count;
-
-            // Assert
-            Assert.AreEqual(cantidadInicial + 1, cantidadFinal);
-        }
-
-        [TestMethod]
-        public void EliminarCliente_DisminuyeLaCantidadDeClientes()
-        {
-            // Arrange
-            var fachada = new Fachada();
-            fachada.CrearCliente("Cliente a borrar", "Test", "456", "b@test.com");
-            var idParaBorrar = fachada.VerTodosLosClientes()[0].Id;
-            int cantidadInicial = fachada.VerTodosLosClientes().Count;
-
-            // Act
-            fachada.EliminarCliente(idParaBorrar);
-            int cantidadFinal = fachada.VerTodosLosClientes().Count;
-
-            // Assert
-            Assert.AreEqual(cantidadInicial - 1, cantidadFinal);
-        }
-        
-        [TestMethod]
-        public void RegistrarLlamada_AgregaInteraccionAlClienteCorrecto()
-        {
-            // Arrange
-            var fachada = new Fachada();
-            fachada.CrearCliente("Cliente con llamada", "Test", "789", "c@test.com");
-            var cliente = fachada.VerTodosLosClientes()[0];
-            
-            // Act
-            fachada.RegistrarLlamada(cliente.Id, System.DateTime.Now, "Tema de prueba", "Entrante");
-            
-            // Assert
-            Assert.AreEqual(1, cliente.Interacciones.Count);
-            Assert.IsInstanceOfType(cliente.Interacciones[0], typeof(Llamada));
-        }
-    }
 }
