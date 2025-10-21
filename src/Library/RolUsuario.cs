085cfb3f4c64d0e314ad997c04545bc73d9374fc
Anthony-Vi <ANTHONY.VIERA@correo.ucu.edu.uy>
2025-10-20 23:46:20 -0300

    Enhance comments in RolUsuario enumeration
    Added comments to clarify the purpose of the RolUsuario enum.

--- src/Library/RolUsuario.cs
+++ src/Library/RolUsuario.cs
@@ -1,7 +1,13 @@
-﻿using Library;
+using Library;
 
+// Esto es una Enumeración (enum).
+// Define un tipo de dato 'RolUsuario' que solo puede tener uno
+// de los valores listados abajo (como si fueran opciones predefinidas).
 public enum RolUsuario
 {
+    // Un usuario que puede configurar el sistema, crear otros usuarios, etc.
     Administrador,
+
+    // Un usuario que interactúa con los clientes y registra ventas.
     Vendedor
 }
